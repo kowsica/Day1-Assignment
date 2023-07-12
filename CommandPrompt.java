eb63ea9c62756465dc2af05e50494406a66fad54
kowsica <kowsica97@gmail.com>
2023-07-12 09:00:54 +0530

    first commit

---
+++ CommandPrompt.java
@@ -0,0 +1,15 @@
+package day1_assignment;
+
+public class CommandPrompt {
+
+	public static void main(String[] args) {
+	int num =0;
+	for(int i =0;i<args.length;i++) {
+		
+		num+= Integer.parseInt(args[i]);
+		
+	}
+		 System.out.println("The sum is:" +num);
+	}
+
+}
